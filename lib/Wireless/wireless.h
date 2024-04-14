653aa8f6f1a15c7f1ab34c04d4b5552b98de26e3
Russel Bradley <russelbradleytan@gmail.com>
2024-04-14 02:49:19 -0400

    Added wireless settings for gripper and controller

--- lib/Wireless/wireless.h
+++ lib/Wireless/wireless.h
@@ -6,7 +6,8 @@
 #include "dpad.h"
 #include "display.h"
 
-const uint8_t gripperAddr[] = {0xF4, 0x12, 0xFA, 0x40, 0x9A, 0x00};
+// const uint8_t gripperAddr[] = {0xF4, 0x12, 0xFA, 0x40, 0x9A, 0x00};
+const uint8_t gripperAddr[] = {0xEC, 0xDA, 0x3B, 0x41, 0xA2, 0x00}; //address of new test ESP-32
 const uint8_t robotAddr[] = {0xEC, 0xDA, 0x3B, 0x41, 0xB6, 0x14};
 const uint8_t controllerAddr[] = {0xEC, 0xDA, 0x3B, 0x41, 0xB5, 0x84};
 const uint8_t gripperControllerAddr[] = {0xEC, 0xDA, 0x3B, 0x41, 0xA1, 0xB8};
@@ -35,11 +36,27 @@ struct RobotMessage {
     bool operator==(const RobotMessage& other);
 } ;
 
+struct GripperControllerMessage {
+    int command;
+
+    void print();
+    bool operator==(const GripperControllerMessage& other);
+} ;
+
+struct GripperMessage {
+    int state;
+
+    void print();
+    bool operator==(const GripperMessage& other);
+} ;
+
 void onSendData(const uint8_t * mac, esp_now_send_status_t status);
 void onRecvData(const uint8_t * mac, const uint8_t *data, int len);
 void setupWireless();
 bool sendControllerData();
 bool sendRobotData();
+bool sendGripperControllerData();
+bool sendGripperData();
 
 extern const uint8_t * peerAddr;
 extern esp_now_peer_info_t peerInfo;
@@ -47,5 +64,7 @@ extern esp_now_peer_info_t peerInfo;
 extern bool freshWirelessData;
 extern ControllerMessage controllerMessage;
 extern RobotMessage robotMessage;
+extern GripperControllerMessage robotMessage;
+extern GripperMessage robotMessage;
 
 #endif // WIRELESS_H
