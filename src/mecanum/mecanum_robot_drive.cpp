460bd3694698668907ab0860ccdc796d1f05bb1c
JamesDMorin <jdmorin@mit.edu>
2024-05-10 13:59:29 -0400

    mecanum specific

--- src/mecanum/mecanum_robot_drive.cpp
+++ src/mecanum/mecanum_robot_drive.cpp
@@ -18,49 +18,56 @@ EncoderVelocity encoders[NUM_MOTORS] = { {ENCODER1_A_PIN, ENCODER1_B_PIN, CPR_31
 PID pids[NUM_MOTORS] = { {Kp, Ki, Kd, 0, pidTau, false}, {Kp, Ki, Kd, 0, pidTau, false}, 
                          {Kp, Ki, Kd, 0, pidTau, false}, {Kp, Ki, Kd, 0, pidTau, false} };
 
-// x, y, phi_i
-double motor_poses[NUM_MOTORS][3] = { {12.2483*25.4/1000, 12.7232*25.4/1000, 2.9214}, { 2.9900*25.4/1000, 7.8362*25.4/1000, -1.5767},
-                                      { 7.1298*25.4/1000, 10.3410*25.4/1000, 0.5002}, {10.4816*25.4/1000, 2.6018*25.4/1000, -2.1484} };
+double velocities[NUM_MOTORS] = {0, 0, 0, 0};
+double controlEfforts[NUM_MOTORS] = {0, 0, 0, 0};
 
-// r, theta
-double motor_position_vectors[NUM_MOTORS][2] = {{sqrt(pow(motor_poses[0][0], 2) + pow(motor_poses[0][1], 2)), atan2(motor_poses[0][1], motor_poses[0][0])}, 
-                                                {sqrt(pow(motor_poses[1][0], 2) + pow(motor_poses[1][1], 2)), atan2(motor_poses[1][1], motor_poses[1][0])}, 
-                                                {sqrt(pow(motor_poses[2][0], 2) + pow(motor_poses[2][1], 2)), atan2(motor_poses[2][1], motor_poses[2][0])}, 
-                                                {sqrt(pow(motor_poses[3][0], 2) + pow(motor_poses[3][1], 2)), atan2(motor_poses[3][1], motor_poses[3][0])}, };
+
 
 double setpoints[NUM_MOTORS] = {0, 0, 0, 0};
 double old_setpoints[NUM_MOTORS] = {0, 0, 0, 0};
-double velocities[NUM_MOTORS] = {0, 0, 0, 0};
-double v_powered[NUM_MOTORS] = {0, 0, 0, 0};
-double controlEfforts[NUM_MOTORS] = {0, 0, 0, 0};
+double v_desired[NUM_MOTORS] = {0, 0, 0, 0};
 
-void setupDrive(){
-    for (uint8_t i = 0; i < NUM_MOTORS; i++)
-        motors[i].setup();
-}
+// x, y, phi_i. Conversion included from inches to meters
+double motor_poses[NUM_MOTORS][3] = { {12.2483*25.4/1000, 12.7232*25.4/1000,  2.9214}, 
+                                      { 2.9900*25.4/1000,  7.8362*25.4/1000, -1.5767},
+                                      { 7.1298*25.4/1000, 10.3410*25.4/1000,  0.5002}, 
+                                      {10.4816*25.4/1000,  2.6018*25.4/1000, -2.1484} };
 
+/**
+ * Updates the setpoints for the angular velocity of each wheel's motor given a set of input target velocities.
+ *
+ * @param forward, sideways Vehicle velocity along vehicle coordinate frame axes in m/s.
+ * @param rotation Vehicle angular velocity in rad/s
+ */
 void updateSetpoints(double forward, double sideways, double rotation) {
     
     double motor_magnitude_angle[NUM_MOTORS][2];
     for (uint8_t i = 0; i < NUM_MOTORS; i++) {
-        double rotational_velocity[2] = {-motor_poses[i][1]*rotation, motor_poses[i][0]*rotation}; // 90 degree ccw rotation matrix
-        double motor_forward = forward + rotational_velocity[0];
-        double motor_sideways = sideways + rotational_velocity[1];
         
-        motor_magnitude_angle[i][1] = atan2(motor_sideways, motor_forward);
+        // Equation 3.1
+        double motor_forward = forward - motor_poses[i][1] * rotation;
+        double motor_sideways = sideways + motor_poses[i][0] * rotation;
+        
+        // Equation 3.2
         motor_magnitude_angle[i][0] = sqrt(pow(motor_forward, 2) + pow(motor_sideways, 2));
+        // Equation 3.3
+        motor_magnitude_angle[i][1] = atan2(motor_sideways, motor_forward);
         
-        // Serial.printf("Motor %u: mag: %.2f, angle: %.2f\n", i, motor_magnitude_angle[i][0], motor_magnitude_angle[i][1]);
+        // Equation 3.5
+        v_desired[i] = motor_magnitude_angle[i][0] * cos(motor_magnitude_angle[i][1] - motor_poses[i][2]);
         
-        v_powered[i] = motor_magnitude_angle[i][0] * cos(motor_magnitude_angle[i][1] - motor_poses[i][2]);
-        
-        // alpha low pass filter
-        setpoints[i] = M_ALPHA * v_powered[i]/R_EFF + (1-M_ALPHA) * old_setpoints[i];
+        // Equation 3.7 with an alpha low pass filter
+        setpoints[i] = M_ALPHA * v_desired[i]/R_EFF + (1-M_ALPHA) * old_setpoints[i];
 
         old_setpoints[i] = setpoints[i];
         Serial.printf("Motor %u: Setpoint: %.2f, ", i, setpoints[i]);
     }
     Serial.println();
+}
+
+void setupDrive(){
+    for (uint8_t i = 0; i < NUM_MOTORS; i++)
+        motors[i].setup();
 }
 
 void updatePIDs() {
--- src/mecanum/mecanum_robot_motion_control.cpp
+++ src/mecanum/mecanum_robot_motion_control.cpp
@@ -5,27 +5,11 @@
 #include "wireless.h"
 #include "mecanum_robot_motion_control.h"
 #include "mecanum_robot_pinout.h"
-#include "EulerAngles.h"
 
-// #define UTURN
-// #define CIRCLE
-#define JOYSTICK
-// #define YOUR_TRAJECTORY
-
-extern RobotMessage robotMessage;
 extern ControllerMessage controllerMessage;
 
-int state = 0;
-double robotVelocity = 0; // velocity of robot, in m/s
-double k = 0; // k is 1/radius from center of rotation circle
-
-extern EncoderVelocity encoders[NUM_MOTORS];
-
-
-// Makes robot follow a trajectory
 void followTrajectory() {
 
-    #ifdef JOYSTICK
     if (freshWirelessData) {
         // Serial.printf("Joystick1: %.2f, %.2f, Joystick2: %.2f, %.2f, BL: %u, BR:%u\n",
         //                 controllerMessage.joystick1.x, controllerMessage.joystick1.y,
@@ -39,67 +23,11 @@ void followTrajectory() {
 
         double sideways = abs(controllerMessage.joystick1.x) < 0.1 ? 0 : -mapDouble(abs(controllerMessage.joystick1.x), 0.1, 1, 0, MAX_FORWARD);
         sideways = controllerMessage.joystick1.x > 0 ? sideways : -sideways;
-        
+
         double rotation = abs(controllerMessage.joystick2.x) < 0.1 ? 0 : -mapDouble(abs(controllerMessage.joystick2.x), 0.1, 1, 0, MAX_ROTATE);
         rotation = controllerMessage.joystick2.x > 0 ? rotation : -rotation;
         // Serial.printf("forward (x): %.2f, sideways (y): %.2f, rotation (theta): %.2f\n", forward, sideways, rotation);
         updateSetpoints(forward, sideways, rotation);
     }
-    #endif 
-
-    #ifdef CIRCLE
-    updateSetpoints(0.0, 0.0, 3.0);
-    #endif 
-
-    #ifdef UTURN
-    switch (state) {
-        case 0: 
-            // Until robot has achieved a x translation of 1m
-            if (robotMessage.x <= 1.0) {
-                // Move in a straight line forward
-                robotVelocity = 0.2;
-                k = 0;
-            } else {
-                // Move on to next state
-                state++;
-            }
-            break;
-
-        case 1:
-            // Until robot has achieved a 180 deg turn in theta
-            if (robotMessage.theta <= M_PI) {
-                // Turn in a circle with radius 25cm 
-                robotVelocity = 0.2;
-                k = 1/0.25;
-            } else {
-                state++;
-            }
-            break;
-
-        case 2:
-            // Until robot has achieved a x translation of -1m
-            if (robotMessage.x >= 0) {
-                // Move in a straight line forward
-                robotVelocity = 0.2;
-                k = 0;
-            } else {
-                // Move on to next state
-                state++;
-            }
-            break;
-
-        default: 
-            // If none of the states, robot should just stop
-            robotVelocity = 0;
-            k = 0;
-            break;
-    }
-    #endif
-
-    #ifdef YOUR_TRAJECTORY
-    // TODO: Create a state machine to define your custom trajectory!
-
-    #endif 
-
 }
 
